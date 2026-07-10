0e200128c6ff581d21cb776bfe856b714207ba64
Fox Danger Piacenti <fox@opencraft.com>
2026-07-10 16:38:34 -0500

    chore: bump version

--- edit_links/__init__.py
+++ edit_links/__init__.py
@@ -2,6 +2,6 @@
 Add Edit links to course content that allows students to contribute changes to FOSS courses.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 default_app_config = 'edit_links.apps.EditLinksConfig'  # pylint: disable=invalid-name
